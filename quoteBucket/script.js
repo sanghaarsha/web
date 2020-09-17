7a1f2f23d83b0bec7859a98384d89be69065757f
sanghaarsha <sanghaarsha@gmail.com>
2020-09-17 10:51:56 +0545

    script fix

--- quoteBucket/script.js
+++ quoteBucket/script.js
@@ -7,11 +7,12 @@ const loader = document.getElementById("loader");
 
 //Get Quote From API
 async function getQuote() {
+    const proxyUrl = "https://ancient-hamlet-75603.herokuapp.com/";
     const apiUrl =
         "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json";
 
     try {
-        const response = await fetch(apiUrl);
+        const response = await fetch(proxyUrl + apiUrl);
         const data = await response.json();
 
         // If author is blank add 'Unknown'
