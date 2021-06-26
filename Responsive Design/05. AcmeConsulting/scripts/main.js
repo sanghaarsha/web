881223ce37693c524804389e559fbda92f156c1a
sanghaarsha <sanghaarsha@gmail.com>
2021-06-26 23:08:52 +0545

    smooth scroll & transparent navbar

--- Responsive Design/05. AcmeConsulting/scripts/main.js
+++ Responsive Design/05. AcmeConsulting/scripts/main.js
@@ -1,3 +1,4 @@
+// Google Map Integration
 let map;
 
 function initMap() {
@@ -6,3 +7,28 @@ function initMap() {
         zoom: 8,
     });
 }
+
+// Transparent Navbar during Scroll
+window.addEventListener("scroll", function () {
+    if (window.scrollY > 150) {
+        document.querySelector("#navbar").style.opacity = 0.8;
+    } else {
+        document.querySelector("#navbar").style.opacity = 1;
+    }
+});
+
+// Smooth Scrolling
+$("#navbar a, .btn").on("click", function (event) {
+    if (this.hash !== "") {
+        event.preventDefault();
+
+        const hash = this.hash;
+
+        $("html, body").animate(
+            {
+                scrollTop: $(hash).offset().top - 100,
+            },
+            800
+        );
+    }
+});
